5f0aaf30c3c82f738d8f5bbf2ceec9364655dd10
naaiyy <naaiyyyy@gmail.com>
2025-10-29 02:08:41 +0100

    update with brand icons

--- src/icons/brand/Google.tsx
+++ src/icons/brand/Google.tsx
@@ -3,7 +3,7 @@ import type { IconProps } from '../../core/types';
 
 export function Google(props: IconProps) {
   return (
-    <IconBase {...props} viewBox="0 0 268.1522 273.8827">
+    <IconBase {...props}>
       <defs>
     <linearGradient id="a">
       <stop offset="0" stopColor="#0fbc5c"/>
